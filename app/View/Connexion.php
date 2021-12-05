ef85b1721f930c63f436e29996de0d002aa7ec18
Idriss91130 <idriss.fall@hetic.net>
2021-12-05 16:52:57 +0100

    Ajouts des post

--- app/View/Connexion.php
+++ app/View/Connexion.php
@@ -72,8 +72,6 @@ if(isset($_POST['connexion'])){
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>  
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-    <script src='main.js'></script>
-    
 </head>
 
 <body class="p-3 mb-2 text-white">
--- app/View/affichePoste.php
+++ app/View/affichePoste.php
@@ -6,8 +6,7 @@
     <title>GACTI</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel='stylesheet' type='text/css' media='screen' href='style1.css'>
-    <script src='main.js'></script>
+    <link rel='stylesheet' type='text/css' media='screen' href='../Styles/style1.css'>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>  
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
@@ -34,7 +33,7 @@
     if(!$mysqli){
         echo "il y a une erreur de connexion avec la base de données";
     } else{
-        $req1 = "SELECT * FROM ANIMATION";
+        $req1 = "SELECT * FROM POST";
         $reponse1 = mysqli_query($mysqli ,$req1);
     }
     if($reponse1){
@@ -49,29 +48,24 @@
         echo "<td colspan=3>".$ligne['Titre']."</td>";
         echo "</tr>";
 
-        echo "<tr >";
-        echo "<td>Titre : ".$ligne['Contenue']."</td>";
-        echo "<td>Jusqu'au : ".$ligne['Auteur']."</td>";
-        echo "<td>Durée : ".$ligne['Date']."h</td>";
+        echo "<tr>";
+        echo "<td colspan=3>".$ligne['Contenu']."</td>";
         echo "</tr>";
-        
-        echo "<tr>";
-        echo "<td>Tarif : ".$ligne['TARIFANIM']." </td>";
-        echo "<td>Age minimun : ".$ligne['LIMITEAGE']."</td>";
-        echo "<td>Nombre de place : ".$ligne['NBREPLACEANIM']."</td>";
-        echo "</tr></br>";
-        
-        echo "<tr>";
-        echo "<td colspan=25% ><a colspan=3 href='Consulter_Activiter.php?cd_Anime=".$ligne['CODEANIM']."'>Voir Activité</a></td>";
-        echo "</tr></br>";
-        
+
+        echo "<tr >";
+        echo "<td>Ecrit par : ".$ligne['Auteur']."</td>";
+        echo "<td>Date : ".$ligne['Date']."</td>";
+        echo "</tr>";
+
         echo "</table>";
+
+        echo "</br>";
     
         
     }
 }else{
         echo "erreur dans l'execution de la requete";
-        echo "le message d'erreur est : " .mysql_error($mysqli);
+        echo "le message d'erreur est : " .mysqli_error($mysqli);
 }
     ?>
         
